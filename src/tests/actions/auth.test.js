a7d1a52e4b76b1d25dd3b559ad3f43d739cdc3ad
Ryo Masuda <mrra4611@gmail.com>
2018-02-22 16:24:32 +0900

    init commit

---
+++ src/tests/actions/auth.test.js
@@ -0,0 +1,15 @@
+import { login, startLogin, logout, startLogout } from '../../actions/auth';
+
+test('should set up login action', () => {
+  const uid = '123abc';
+  const action = login(uid);
+  expect(action).toEqual({
+      type: 'LOGIN',
+      uid
+  });
+});
+
+test('should set up logout action correctly', () => {
+  const action = logout();
+  expect(action).toEqual({type: 'LOGOUT'});
+});
